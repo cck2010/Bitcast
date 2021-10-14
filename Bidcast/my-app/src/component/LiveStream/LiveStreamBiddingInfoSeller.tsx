3279df949862d98db4daec107f28a0d1f1fb93c9
Szeto Pak Hin <flyers100@gmail.com>
2021-10-15 02:02:31 +0800

    LiveStream Panel

---
+++ Bidcast/my-app/src/component/LiveStream/LiveStreamBiddingInfoSeller.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LiveStreamBiddingInfoSeller() {
+    return <div className="LiveStreamBiddingInfoSeller"></div>;
+}
+
+export default LiveStreamBiddingInfoSeller;
--- Bidcast/my-app/src/component/LiveStream/LiveStreamControlPanel.tsx
+++ Bidcast/my-app/src/component/LiveStream/LiveStreamControlPanel.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import Carousel from "react-tiny-slider";
 import { TinySliderInstance } from "tiny-slider";
+import LiveStreamBiddingInfoSeller from "./LiveStreamBiddingInfoSeller";
 
 function LiveStreamControlPanel() {
     const carousel = useRef<TinySliderInstance>(null);
@@ -11,6 +12,8 @@ function LiveStreamControlPanel() {
     interface Product {
         id: number;
         src: string;
+        name: string;
+        price: number;
         isSold: boolean;
         isSelected: boolean;
     }
@@ -19,24 +22,32 @@ function LiveStreamControlPanel() {
         {
             id: 0,
             src: "https://cdn.shopify.com/s/files/1/0339/7091/3412/products/POPMARTWinniethePooh.jpg",
+            name: "pooh1",
+            price: 10,
             isSold: false,
             isSelected: false,
         },
         {
             id: 1,
             src: "https://lumiere-a.akamaihd.net/v1/images/c94eed56a5e84479a2939c9172434567c0147d4f.jpeg",
+            name: "pooh2",
+            price: 20,
             isSold: false,
             isSelected: false,
         },
         {
             id: 2,
             src: "https://winniethepoohshow.com/assets/img/WTP-PoohHoneypot-Placeholder.png",
+            name: "pooh3",
+            price: 30,
             isSold: false,
             isSelected: false,
         },
         {
             id: 3,
             src: "https://www.rd.com/wp-content/uploads/2020/01/shutterstock_247528582-2-copy-scaled.jpg",
+            name: "pooh4",
+            price: 40,
             isSold: false,
             isSelected: false,
         },
@@ -48,7 +59,7 @@ function LiveStreamControlPanel() {
                 <div className="col-6 d-flex d-col carousel">
                     <Carousel
                         swipeAngle={false}
-                        items={3}
+                        items={1}
                         ref={carousel}
                         controls={false}
                         nav={false}
@@ -58,8 +69,9 @@ function LiveStreamControlPanel() {
                                 return;
                             }
                             let ind = parseInt(
-                                info.slideItems[slideIndex].alt
-                                    .split("pic")
+                                info.slideItems
+                                    .item(slideIndex)
+                                    .ariaLabel.split("card")
                                     .join("")
                             );
                             newProducts = newProducts.concat(
@@ -76,14 +88,31 @@ function LiveStreamControlPanel() {
                         }}
                     >
                         {products.map((product) => (
-                            <img
+                            <div
                                 key={product.id}
-                                className={`carousel_img ${
-                                    product.isSold ? "sold " : ""
-                                } ${product.isSelected ? "selected" : ""}`}
-                                src={product.src}
-                                alt={`pic${product.id}`}
-                            />
+                                className={`carousel_card`}
+                                aria-label={`card${product.id}`}
+                            >
+                                <img
+                                    key={product.id}
+                                    className={`carousel_img ${
+                                        product.isSold ? "sold " : ""
+                                    } ${product.isSelected ? "selected" : ""}`}
+                                    src={product.src}
+                                    alt={`pic${product.id}`}
+                                />
+                                <div className="product_info">
+                                    <div className="product_name">
+                                        競價項目:
+                                        <br />
+                                        {product.name}
+                                    </div>
+                                    <div className="product_price">
+                                        起標價:
+                                        <br />${product.price}
+                                    </div>
+                                </div>
+                            </div>
                         ))}
                     </Carousel>
                     <button
@@ -99,7 +128,9 @@ function LiveStreamControlPanel() {
                         <i className="fas fa-caret-right"></i>
                     </button>
                 </div>
-                <div className="col-6">Control Panel</div>
+                <div className="col-6">
+                    <LiveStreamBiddingInfoSeller></LiveStreamBiddingInfoSeller>
+                </div>
             </div>
         </div>
     );
