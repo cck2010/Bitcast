869ac13a33a2b5c19777fb24b4b106373b259989
Enki <hunkzan@gmail.com>
2021-10-17 15:07:16 +0800

    CreateBids reducer and Add CreateBids state

--- Bidcast/my-app/src/store.ts
+++ Bidcast/my-app/src/store.ts
@@ -9,6 +9,8 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import { userReducer, UserState } from "./redux/user/reducer";
 import thunk, { ThunkDispatch } from "redux-thunk";
 import { UserActions } from "./redux/user/actions";
+import { CreateBids, createBidsReducer } from "./redux/createbid/reducer";
+import { productsReducer, ProductsState } from "./redux/products/reducer";
 
 export const history = createBrowserHistory();
 
@@ -17,12 +19,16 @@ export type RootAction = RouterAction | UserActions;
 export type RootThunkDispatch = ThunkDispatch<RootState, null, RootAction>;
 
 export interface RootState {
-    user: UserState;
-    router: RouterState;
+    user: UserState,
+    router: RouterState,
+    CreateBids: CreateBids,
+    products: ProductsState
 }
 
 const reducer = combineReducers<RootState>({
     user: userReducer,
+    CreateBids: createBidsReducer,
+    products: productsReducer,
     router: connectRouter(history),
 });
 
