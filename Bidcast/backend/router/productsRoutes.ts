fec1412d30d7d73822676598218a1a2d5839fd1c
Enki <hunkzan@gmail.com>
2021-10-19 15:30:39 +0800

    product Route fix

--- Bidcast/backend/router/productsRoutes.ts
+++ Bidcast/backend/router/productsRoutes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request} from "express";
 import multer from "multer";
 import path from "path";
 import { productsController } from "../server";
