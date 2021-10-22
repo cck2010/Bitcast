9f8e7363d5eda1d9fee6fbadbe44972707ff76ea
Steven So <sochunkit3@gmail.com>
2021-10-22 15:12:55 +0800

    ion-sfu server updated

--- Bidcast/backend/service/comingAuctionService.ts
+++ Bidcast/backend/service/comingAuctionService.ts
@@ -6,9 +6,9 @@ export class ComingAuctionService {
     getComingAuction = async () => {
         const results = await this.knex.raw(
             /*sql*/
-            `select * from products 
-            left outer join users on products.seller_id = users.id 
-            limit 5;`
+            `select * from live 
+            left outer join users on live.user_id = users.id 
+            limit 6;`
         )
         return {
             success: true,
