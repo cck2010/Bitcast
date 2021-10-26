84995a07e20bd5d7ca254c0e699402e3ffb7c98a
Pak Hin <flyers100@gmail.com>
2021-10-26 18:35:30 +0800

    navbar

--- Bidcast/my-app/src/pages/homepage/Navbar.tsx
+++ Bidcast/my-app/src/pages/homepage/Navbar.tsx
@@ -86,7 +86,6 @@ export function HomePageNavbar() {
                     <img
                         alt="bidcast_logo"
                         src={bidcast_logo}
-                        width="40"
                         height="40"
                         className="d-inline-block align-top"
                     />
