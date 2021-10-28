3c5656660a5d042a266f82e869faf1f097d1b71d
Szeto Pak Hin <flyers100@gmail.com>
2021-10-28 22:04:49 +0800

    livestream bug fix

--- Bidcast/my-app/src/component/LiveStream/ErrorPriceTooLow.tsx
+++ Bidcast/my-app/src/component/LiveStream/ErrorPriceTooLow.tsx
@@ -3,7 +3,11 @@ import { CToast, CToastBody, CToastClose } from "@coreui/react";
 
 function ErrorPriceTooLow() {
     return (
-        <CToast autohide={true} visible={true} className="align-items-center">
+        <CToast
+            autohide={true}
+            visible={true}
+            className="align-items-center mw-100"
+        >
             <div className="d-flex text-danger">
                 <CToastBody>你的叫價太低</CToastBody>
                 <CToastClose className="me-2 m-auto" />
